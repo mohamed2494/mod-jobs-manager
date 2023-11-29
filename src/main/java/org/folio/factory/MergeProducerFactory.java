9683be9f793c69c1e2cbb67230fb7dd9b3abb37d
ramadan <m.ramadan@koinz.app>
2023-11-30 00:04:08 +0200

    factory should return Type C Merge

--- src/main/java/org/folio/factory/MergeProducerFactory.java
+++ src/main/java/org/folio/factory/MergeProducerFactory.java
@@ -3,28 +3,33 @@ package org.folio.factory;
 import org.folio.service.merge.AbstractMergeProducerService;
 import org.folio.service.merge.TypeAMergeProducerService;
 import org.folio.service.merge.TypeBMergeProducerService;
+import org.folio.service.merge.TypeCMergeProducerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MergeProducerFactory {
-    private final TypeAMergeProducerService typeAMergeService;
-    private final TypeBMergeProducerService typeBMergeService;
+  private final TypeAMergeProducerService typeAMergeService;
+  private final TypeBMergeProducerService typeBMergeService;
 
-    @Autowired
-    public MergeProducerFactory(TypeAMergeProducerService typeAMergeService, TypeBMergeProducerService typeBMergeService) {
-        this.typeAMergeService = typeAMergeService;
-        this.typeBMergeService = typeBMergeService;
+  private final TypeCMergeProducerService typeCMergeService;
+
+  @Autowired
+  public MergeProducerFactory(TypeAMergeProducerService typeAMergeService, TypeBMergeProducerService typeBMergeService, TypeCMergeProducerService typeCMergeProducerService) {
+    this.typeAMergeService = typeAMergeService;
+    this.typeBMergeService = typeBMergeService;
+    this.typeCMergeService = typeCMergeProducerService;
+
+  }
+
+  public AbstractMergeProducerService createMergeProducer(String type) {
+
+    if ("TypeA".equalsIgnoreCase(type)) {
+      return typeAMergeService;
+    } else if ("TypeB".equalsIgnoreCase(type)) {
+      return typeBMergeService;
     }
-
-    public AbstractMergeProducerService createMergeProducer(String type) {
-
-        if ("TypeA".equalsIgnoreCase(type)) {
-            return typeAMergeService;
-        } else if ("TypeB".equalsIgnoreCase(type)) {
-            return typeBMergeService;
-        }
-        // Handle unknown merge types or return a default producer
-        return typeAMergeService;
-    }
+    // Handle unknown merge types or return a default producer
+    return typeCMergeService;
+  }
 }
